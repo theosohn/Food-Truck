1b2aa35d2deaf63e0741619ca0727e937fccea88
theosohn <theosohn@berkeley.edu>
2025-03-20 03:06:54 -0700

    Qualtrics testing

--- scripts/utils/updateData.js
+++ scripts/utils/updateData.js
@@ -1,6 +1,3 @@
 export function updateData(s) {
-    const fs = require('fs');
-    fs.appendFile('../data.txt', s, function (err) {
-        if (err) throw err;
-    });
+    window.getElementById("qualtricsString").textContent = s;
 }
