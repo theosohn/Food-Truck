f0cd246f8027c201851cc02c1bc947a22f839862
theosohn <theosohn@berkeley.edu>
2025-04-02 02:02:38 -0700

    Revert

--- scripts/utils/updateData.js
+++ scripts/utils/updateData.js
@@ -1,8 +1,8 @@
 export function updateData(s) {
-    var fs = require('fs');
+    /*var fs = require('fs');
     fs.appendFile('../data.txt', s, function (err) {
         if (err) throw err;
-    });
+    });*/
 }
 /*
 import { app, firestore } from '.../firebaseConfig.js';
