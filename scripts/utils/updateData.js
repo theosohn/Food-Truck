a7c473fc3e47fe4cf5984f0a093d1052ef48bbb5
theosohn <theosohn@berkeley.edu>
2025-04-03 09:09:47 -0700

    Data collecting string test 5

--- scripts/utils/updateData.js
+++ scripts/utils/updateData.js
@@ -1,6 +1,6 @@
 export function updateData(value) {
-    const textElement = document.getElementById('qualtricsString');
-    textElement.textContent = value;
+    /*const textElement = document.getElementById('qualtricsString');
+    textElement.textContent = value;*/
 }
 
 /*
